300bab3806758102971f4c07f104c92a6152475b
java <sandlovi@gmail.com>
2023-07-19 10:20:33 +0100

    servidor api

---
+++ public/JAVASCRIPT/js/sintaxis_prova.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Setup
+function testEqual(val) {
+  if (val==12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+console.log(testEqual(12));
+
+
+
+/*
+function variables() {
+  //alert('hola');
+  console.log('Hola a todos');
+}
+variables();*/
